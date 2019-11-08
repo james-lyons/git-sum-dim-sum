cf03c3aa5161d7527b42aa2830893f2297d95b7e
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-11-08 12:57:06 -0800

    fix delete review

--- middleware/adminRequired.js
+++ middleware/adminRequired.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (!req.session.currentUser || req.session.currentUser.role !== admin) {
+    if (!req.session.currentUser || req.session.currentUser.role !== 'admin') {
         return res.status(401).json({
             status: 401,
             message: 'Unauthorized. Please login and try again'
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,7 @@ const authRequired = require('../middleware/authRequired');
 
 router.get('/', ctrls.reviewsCtrl.indexReviews);
 router.post('/', authRequired, ctrls.reviewsCtrl.createReview)
-router.delete('/:id', authRequired, ctrls.reviewsCtrl.deleteReview)
 router.put('/:id', authRequired, ctrls.reviewsCtrl.editReview)
+router.delete('/:id', authRequired, ctrls.reviewsCtrl.deleteReview)
 
 module.exports = router;
